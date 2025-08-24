e88b7c015ad3a4ab206f38c5907b8e8d683231e9
Rekha0404 <rekhachopra969@gmail.com>
2025-08-24 17:10:01 +0530

    Create 1_first.java

---
+++ 1_first.java
@@ -0,0 +1,18 @@
+public class first {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+    }
+}
+
+
+// Explanation
+// public: access specifier means accessible anywhere
+// class first: a class with name first is madded
+
+// inside class{}
+// static: method belongs to the class, so it can be called without creating an object
+// main: function (entry point or from where execution starts)
+// String args[]: allows the program to accept command-line arguments
+
+// System: built-in class that does system-related works
+// out: object of system class that refers to output
